86f023593b70a0a8d94e97930c953f5ca473a7f2
Adrian Bacab <bacabadrian7@gmail.com>
2023-04-23 23:30:26 -0600

    primer commit

---
+++ src/git_práctica.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class git_práctica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Escriba imprimir para ver el nombre");
+		Scanner io = new Scanner(System.in);
+		
+		String actionString = io.nextLine();
+		if (actionString.equals("imprimir")) {
+			imprimir();
+				}
+
+	}
+public static void imprimir() {
+	System.out.println("Mi nombre es Adrian Cocom Bacab");	
+	}
+
+
+}
